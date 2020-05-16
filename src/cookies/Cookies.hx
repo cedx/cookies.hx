e32f56bf08b95cce3529a15f1a217ba5b804e3a6
Cédric Belin <cedric@belin.io>
2020-05-16 19:43:01 +0200

    Implemented the `Cookies` class

---
+++ src/cookies/Cookies.hx
@@ -0,0 +1,229 @@
+package cookies;
+
+import haxe.DynamicAccess;
+import haxe.Json;
+import js.Browser;
+import js.Syntax;
+import js.html.EventTarget;
+import js.html.StorageEvent;
+import js.lib.Object;
+import js.lib.Symbol;
+import js.lib.Error.TypeError;
+
+using StringTools;
+
+/** Provides access to the [HTTP Cookies](https://developer.mozilla.org/en-US/docs/Web/HTTP/Cookies). **/
+@:expose
+@:require(js)
+class Cookies extends EventTarget {
+
+  /** The default cookie options. **/
+  public var defaults(default, null): CookieOptions;
+
+  /** The keys of the cookies associated with the current document. **/
+  public var keys(get, never): Array<String>;
+
+  /** The number of cookies associated with the current document. **/
+  public var length(get, never): Int;
+
+  /** Creates a new cookie service. **/
+  public function new(?defaults: CookieOptions) {
+    super();
+    this.defaults = defaults != null ? defaults : new CookieOptions();
+  }
+
+  /** Gets the keys of the cookies associated with the current document. **/
+  function get_keys(): Array<String> {
+    final keys = Browser.document.cookie.replace(~/((?:^|\s*;)[^=]+)(?=;|$)|^\s*|\s*(?:=[^;]*)?(?:\1|$)/g, '');
+    return keys.length ? keys.split(~/\s*(?:=[^;]*)?;\s*/).map(StringTools.urlDecode) : [];
+  }
+
+  /** Gets the number of cookies associated with the current document. **/
+  function get_length(): Int
+    return keys.length;
+
+  /** Removes all cookies associated with the current document. **/
+  public function clear(): Void {
+    for (key in keys) removeItem(key);
+    emit(null, null, null);
+  }
+
+  /**
+    Gets the value associated to the specified `key`.
+    Returns the given `defaultValue` if the cookie does not exist.
+  **/
+  public function get(key: String, ?defaultValue: String): Null<String> {
+    if (!has(key)) return defaultValue;
+
+    try {
+      final token = key.urlEncode().replace(~/[-.+*]/g, '\\$&');
+      final scanner = new EReg('(?:(?:^|.*;)\\s*$token\\s*=\\s*([^;]*).*$)|^.*$');
+      return Browser.document.cookie.replace(scanner, '$1').urlDecode();
+    }
+
+    catch (err) {
+      return defaultValue;
+    }
+  }
+
+  /**
+    Gets the deserialized value associated to the specified `key`.
+    Returns the given `defaultValue` if the cookie does not exist.
+  **/
+  public function getObject(key: String, ?defaultValue: Any): Null<Dynamic> {
+    try {
+      final value = get(key);
+      return value != null ? Json.parse(value) : defaultValue;
+    }
+
+    catch (err) {
+      return defaultValue;
+    }
+  }
+
+  /** Gets a value indicating whether the current document has a cookie with the specified `key`. **/
+  public function has(key: String): Bool {
+    final token = key.urlEncode().replace(~/[-.+*]/g, '\\$&');
+    return new EReg('(?:^|;\\s*)$token\\s*=').test(Browser.document.cookie);
+  }
+
+  /** Returns a new iterator that allows iterating the cookies associated with the current document. **/
+  public function keyValueIterator(): KeyValueIterator<String, String>
+    return new CookieIterator(this);
+
+  /**
+    Looks up the cookie with the specified `key`, or add a new cookie if it isn't there.
+
+    Returns the value associated to `key`, if there is one.
+    Otherwise calls `ifAbsent` to get a new value, associates `key` to that value, and then returns the new value.
+  **/
+  public function putIfAbsent(key: String, ifAbsent: () -> String, ?options: CookieOptions): String {
+    if (!has(key)) set(key, ifAbsent(), options);
+    return get(key);
+  }
+
+  /**
+    Looks up the cookie with the specified `key`, or add a new cookie if it isn't there.
+
+    Returns the deserialized value associated to `key`, if there is one.
+    Otherwise calls `ifAbsent` to get a new value, serializes and associates `key` to that value, and then returns the new value.
+  **/
+  public function putObjectIfAbsent(key: String, ifAbsent: () -> Any, ?options: CookieOptions): Dynamic {
+    if (!has(key)) setObject(key, ifAbsent(), options);
+    return getObject(key);
+  }
+
+  /**
+    Removes the cookie with the specified `key` and its associated value.
+    Returns the cookie with the specified `key` before it was removed.
+  **/
+  public function remove(key: String, ?options: CookieOptions): Null<String> {
+    final oldValue = get(key);
+    removeItem(key, options);
+    emit(key, oldValue, null);
+    return oldValue;
+  }
+
+  /**
+    Associates a given `value` to the specified `key`.
+    Returns this instance, throws a `TypeError` if specified key is invalid.
+  **/
+  public function set(key: String, value: String, ?options: CookieOptions): Cookies {
+    if (!key.length) throw new TypeError('Invalid cookie name.');
+
+    final cookieOptions = getOptions(options).toString();
+    var cookieValue = '${key.urlEncode()}=${value.urlEncode()}';
+    if (cookieOptions.length) cookieValue += '; $cookieOptions';
+
+    final oldValue = get(key);
+    Browser.document.cookie = cookieValue;
+    emit(key, oldValue, value);
+    return this;
+  }
+
+  /**
+    Serializes and associates a given `value` to the specified `key`.
+    Returns this instance.
+  **/
+  public function setObject(key: String, value: Any, ?options: CookieOptions): Cookies
+    return set(key, Json.stringify(value), options);
+
+  /** Converts this object to a map in JSON format. **/
+  public function toJSON(): DynamicAccess<String> {
+    final map: DynamicAccess<String> = {};
+    for (key => value in this) map[key] = value;
+    return map;
+  }
+
+  /** Returns a string representation of this object. **/
+  public function toString(): String
+    return Browser.document.cookie;
+
+  /** Emits a new cookie event. **/
+  private function emit(key: Null<String>, oldValue: Null<String>, newValue: Null<String>): Void
+    dispatchEvent(new StorageEvent('change', {
+      key: key,
+      newValue: newValue,
+      oldValue: oldValue,
+      storageArea: Browser.document.cookie,
+      url: Browser.location.href
+    }));
+
+  /** Merges the default cookie options with the specified ones. **/
+  private function getOptions(?options: CookieOptions): CookieOptions {
+    if (options == null) options = new CookieOptions();
+    return new CookieOptions({
+      domain: options.domain.length > 0 ? options.domain : defaults.domain,
+      expires: options.expires != null ? options.expires : defaults.expires,
+      path: options.path.length > 0 ? options.path : defaults.path,
+      secure: options.secure ? options.secure : defaults.secure
+    });
+  }
+
+  /** Removes the value associated to the specified `key`. **/
+  private function removeItem(key: String, ?options: CookieOptions): Void {
+    if (!has(key)) return;
+    final cookieOptions = getOptions(options);
+    cookieOptions.expires = Date.fromTime(0);
+    Browser.document.cookie = '${key.urlEncode()}=; $cookieOptions';
+  }
+  
+  /** Initializes the class prototype. **/
+  static function __init__(): Void {
+    var proto = Syntax.field(Cookies, 'prototype');
+    Object.defineProperty(proto, Syntax.field(Symbol, 'iterator'), {
+      value: Syntax.code('function *() { for (const key of this.keys) yield [key, this.get(key)]; }')
+    });
+
+    Object.defineProperties(proto, {
+      keys: {get: proto.get_keys},
+      length: {get: proto.get_length}
+    });
+  }
+}
+
+/** Permits iteration over elements of a `Cookies` instance. **/
+private class CookieIterator {
+
+  /** The instance to iterate. **/
+  private final cookies: Cookies;
+
+  /** The underlying key iterator. **/
+  private final keys: ArrayIterator<String>;
+
+  /** Creates a new cookie iterator. **/
+  public function new(cookies: Cookies) {
+    this.cookies = cookies;
+    keys = new ArrayIterator<String>(cookies.keys);
+  }
+
+  /** Returns a value indicating whether the iteration is complete. **/
+  public function hasNext(): Bool
+    return keys.hasNext();
+
+  /** Returns the current item of the iterator and advances to the next one. **/
+  public function next(): {key: String, value: String} {
+    final key = keys.next();
+    return {key: key, value: cookies.get(key)};
+  }
+}
