415e42ed6aaa80ae92be7adbd8e54f155878b6bd
Cédric Belin <cedric@belin.io>
2022-04-11 22:46:09 +0200

    Enabled the code coverage

--- test/RunTests.hx
+++ test/RunTests.hx
@@ -1,3 +1,4 @@
+import instrument.coverage.Coverage;
 import js.Syntax;
 import tink.testrunner.Reporter.AnsiFormatter;
 import tink.testrunner.Reporter.BasicReporter;
@@ -12,7 +13,8 @@ function main() {
 	]);
 
 	ANSI.stripIfUnavailable = false;
-	Runner
-		.run(tests, new BasicReporter(new AnsiFormatter()))
-		.handle(outcome -> Syntax.code("exit({0})", outcome.summary().failures.length));
+	Runner.run(tests, new BasicReporter(new AnsiFormatter())).handle(outcome -> {
+		Coverage.endCoverage();
+		Syntax.code("exit({0})", outcome.summary().failures.length);
+	});
 }
--- test/RunTests.js
+++ test/RunTests.js
@@ -1,4 +1,5 @@
-const {writeFile} = require("node:fs/promises");
+const {appendFileSync} = require("node:fs");
+const {rm, writeFile} = require("node:fs/promises");
 const {createServer} = require("node:http");
 const {join} = require("node:path");
 const puppeteer = require("puppeteer");
@@ -6,7 +7,10 @@ const handler = require("serve-handler");
 
 // Start the application.
 (async function main() {
-	await writeFile(join(__dirname, "../var/tests.html"), [
+	const basePath = join(__dirname, "../var");
+	const lcovFile = join(basePath, "lcov.info");
+	await rm(lcovFile, {force: true});
+	await writeFile(join(basePath, "tests.html"), [
 		'<!DOCTYPE html>',
 		'<html dir="ltr" lang="en">',
 		'\t<head><meta charset="UTF-8"/></head>',
@@ -14,20 +18,26 @@ const handler = require("serve-handler");
 		'</html>'
 	].join("\n"));
 
-	const server = createServer((req, res) => handler(req, res, {public: join(__dirname, "../var")}));
-	server.listen(8080);
-
 	const browser = await puppeteer.launch();
 	const page = await browser.newPage();
-	page.on("console", message => console.log(message.text()));
 	page.on("pageerror", error => console.error(error));
+	page.on("console", message => {
+		const output = message.text().trim();
+		if (output.startsWith("TN:") && output.endsWith("end_of_record")) appendFileSync(lcovFile, output);
+		else console.log(message.text());
+	});
 
+	const server = createServer((req, res) => handler(req, res, {public: basePath}));
+	server.listen(8080);
 	await page.exposeFunction("exit", async code => {
 		process.exitCode = code;
 		server.close();
-		try { await browser.close(); } catch {}
+		await browser.close();
 	});
 
 	await page.evaluate(() => console.info(navigator.userAgent));
-	await page.goto("http://localhost:8080/tests.html");
+	await Promise.all([
+		page.waitForNavigation(),
+		page.goto("http://localhost:8080/tests.html")
+	]);
 })();
