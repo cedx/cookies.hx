0701e97db89345b7a11efe86c16df833e9dd6d1d
Cédric Belin <cedric@belin.io>
2023-02-15 16:46:27 +0100

    Updated the build system [skip ci]

--- scripts/Dist.hx
+++ scripts/Dist.hx
@@ -1,2 +1,2 @@
 /** Runs the script. **/
-function main() for (script in ["Clean", "Version"]) Sys.command("lix", [script]);
+function main() for (script in ["Clean", "Version"]) Sys.command('lix $script');
--- scripts/Doc.hx
+++ scripts/Doc.hx
@@ -7,9 +7,8 @@ import sys.io.File;
 function main() {
 	if (FileSystem.exists("docs")) Tools.removeDirectory("docs");
 
-	Sys.command("haxe", ["--define", "doc-gen", "--no-output", "--xml", "var/api.xml", "build.hxml"]);
-	Sys.command("lix", [
-		"run", "dox",
+	Sys.command("haxe --define doc-gen --no-output --xml var/api.xml build.hxml");
+	Sys.command("lix", ["run", "dox",
 		"--define", "description", "Service for interacting with the HTTP cookies, in Haxe.",
 		"--define", "source-path", "https://github.com/cedx/cookies.hx/blob/main/src",
 		"--define", "themeColor", "0xffc105",
