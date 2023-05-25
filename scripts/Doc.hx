10466ceff307af04f86ebd8c8c0a37235a601985
Cédric Belin <cedric@belin.io>
2023-05-25 23:29:11 +0200

    Update the dependencies

--- scripts/Doc.hx
+++ scripts/Doc.hx
@@ -13,7 +13,7 @@ function main() {
 		"--define", "source-path", "https://github.com/cedx/cookies.hx/blob/main/src",
 		"--define", "themeColor", "0xea8220",
 		"--define", "version", Version.packageVersion,
-		"--define", "website", "https://github.com/cedx/cookies.hx",
+		"--define", "website", "https://docs.belin.io/cookies.hx",
 		"--input-path", "var",
 		"--output-path", "docs",
 		"--title", "Cookies for Haxe",
