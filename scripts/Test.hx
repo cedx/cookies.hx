fae203024e66fb93054bdeca8542258a3846ea17
Cédric Belin <cedric@belin.io>
2023-07-23 19:01:31 +0200

    Update the "Test" task

--- scripts/Test.hx
+++ scripts/Test.hx
@@ -1,12 +1,12 @@
 /** Runs the test suite. **/
 function main() {
-	var exitCode = 0;
+	var code = 0;
 
 	Sys.println("> Testing with `haxe.Json` serializer...");
-	exitCode = Sys.command("haxe test.hxml");
-	if (exitCode != 0) Sys.exit(exitCode);
+	if (Sys.command("haxe test.hxml") != 0) code++;
 
 	Sys.println("> Testing with `tink.Json` serializer...");
-	exitCode = Sys.command("haxe --library tink_json test.hxml");
-	if (exitCode != 0) Sys.exit(exitCode);
+	if (Sys.command("haxe --library tink_json test.hxml") != 0) code++;
+
+	Sys.exit(code);
 }
