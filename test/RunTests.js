e6c1cd32012491d347b2b45e84b38f052f608e66
Cédric Belin <cedric@belin.io>
2023-10-10 21:30:44 +0200

    Update the tests

--- test/RunTests.js
+++ test/RunTests.js
@@ -4,12 +4,12 @@ const {rm, writeFile} = require("node:fs/promises");
 const {createServer} = require("node:http");
 const {EOL} = require("node:os");
 const process = require("node:process");
-const {firefox} = require("playwright");
+const {chromium} = require("playwright");
 const handler = require("serve-handler");
 
 (async function() {
 	// Start the browser.
-	const browser = await firefox.launch();
+	const browser = await chromium.launch();
 	const coverage = [];
 	const page = await browser.newPage();
 	const server = createServer((req, res) => handler(req, res, {public: "var"}));
